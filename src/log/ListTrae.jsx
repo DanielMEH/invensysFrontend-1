cbc73207cdcca31461b76546e04718c921841381
andresfdel17 <andresfdel13@hotmail.com>
2023-05-24 06:41:16 -0500

    g

--- src/log/ListTrae.jsx
+++ src/log/ListTrae.jsx
@@ -4,20 +4,14 @@ export const ListTrae = () => {
   return (
     <>
     
-        <div class="flex flex-col justify-center items-center ">
+        <div class=" ">
             <div class="relative flex flex-col items-center rounded-[20px] w-[700px]   bg-white bg-clip-border
              shadow-3xl shadow-shadow-500 dark:!bg-navy-800 dark:text-white dark:!shadow-none p-3">
                 <div class="mt-2 mb-8 w-full">
                     <h4 class="px-2 text-xl font-bold text-navy-700 dark:text-white">
-                    General Information
+                    Información general de tu negocio
                     </h4>
-                    <p class="mt-2 px-2 text-base text-gray-600">
-                    As we live, our hearts turn colder. Cause pain is what we go through
-                    as we become older. We get insulted by others, lose trust for those
-                    others. We get back stabbed by friends. It becomes harder for us to
-                    give others a hand. We get our heart broken by people we love, even
-                    that we give them all...
-                    </p>
+                   
                 </div> 
                 <div class="grid grid-cols-2 gap-4 px-2 w-full">
                     <div class="flex flex-col items-start justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 shadow-3xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
