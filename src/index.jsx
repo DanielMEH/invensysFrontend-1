3ad3d5990553746f23bb810c088134d8039168c9
klayngo <jdospina943@misena.edu.co>
2023-05-28 20:37:56 -0500

    async data

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './app/App';
-import { BrowserRouter } from "react-router-dom"
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import 'react-toastify/dist/ReactToastify.css';
-const root = ReactDOM.createRoot(
-  document.getElementById( 'sript-data' ) as HTMLElement
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./app/App";
+import { BrowserRouter } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import "react-toastify/dist/ReactToastify.css";
+const root = ReactDOM.createRoot(document.getElementById("sript-data"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <GoogleOAuthProvider clientId="797595300559-75b5122n29hr1jskrso9lh5fooh05trr.apps.googleusercontent.com">
         <App />
       </GoogleOAuthProvider>
-
     </BrowserRouter>
   </React.StrictMode>
 );
@@ -22,4 +19,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
